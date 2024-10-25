3d2bc9f1161bdc06b16cde57dbe557e21b46fa27
Yarlew <yarlewgabriel03@gmail.com>
2024-10-24 22:22:19 -0300

    erro

--- desafios/codicogos usados.py
+++ desafios/codicogos usados.py
@@ -1,3 +1,4 @@
+import emoji
 nota= float(input('qual foi sua nota?'))
 
 if nota >= 6:
@@ -10,5 +11,6 @@ else:
 #é uma estrutura de condição que permite avaliar uma expressão e, de acordo com seu resultado, executar uma determinada ação.
 
 #isso é uma grande besteira minha
+print(emoji.emojize('Rapaz... que bixo mizeravi mrm :rosto_com_sobrancelha_levantada_emoji:', language='pt'))
 
 
--- desafios/desafio 19.py
+++ desafios/desafio 19.py
@@ -1,1 +1,18 @@
-#crie um pro
+import random
+import emoji
+
+while True:
+   numero_secreto = random.randint(1, 10)
+
+   numero_sugerido=int(input('chute um numero, e tente sua sorte: '))
+
+   if numero_sugerido < numero_secreto:
+        print('Numero Baixo ')
+
+   elif numero_sugerido > numero_secreto:
+        print('Numero Muito Alto')
+
+   else:
+        print('Rapaz... que bixo mizeravi mrm')
+        break
+        
