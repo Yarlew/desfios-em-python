0cf3188501105319907236e981c14ca9828e0d37
Yarlew <yarlewgabriel03@gmail.com>
2024-10-25 21:34:06 -0300

    foi um bom avanço já

--- desafios/desafio 19.py
+++ desafios/desafio 19.py
@@ -1,18 +1,21 @@
-import random
-import emoji
+#essa script sorteia um numero e pede para o usuario  acertar, o programa fale se o chute dele esta quente ou frio
+
+from random import randint
+from termcolor import colored
+
+numero_secreto= randint(1,50)
 
 while True:
-   numero_secreto = random.randint(1, 10)
 
    numero_sugerido=int(input('chute um numero, e tente sua sorte: '))
 
    if numero_sugerido < numero_secreto:
-        print('Numero Baixo ')
+        print(colored('Numero Baixo ', 'blue'))
 
    elif numero_sugerido > numero_secreto:
-        print('Numero Muito Alto')
+        print(colored('Numero Muito Alto','red'))
 
-   else:
-        print('Rapaz... que bixo mizeravi mrm')
+   elif numero_sugerido == numero_secreto:
+        print(colored('Rapaz... que bixo mizeravi mrm', 'green'))
         break
         
