d52115462913b039432bbd5793a1456477b91bb1
Yarlew <yarlewgabriel03@gmail.com>
2024-11-01 13:20:28 -0300

    d

---
+++ desafios/calculadora.py
@@ -0,0 +1,7 @@
+#calcuradora feita 100% sem chat gpt...
+
+operação=input('selecione a operação: ')
+
+while True:
+    if operação == (1):
+        
--- desafios/desafio 20.py
+++
@@ -1,7 +0,0 @@
-lista_desatualizada=input('digite os nomes utilizando virgula apos cada nome: ')
-
-lista_desatualizada.sort()
-
-print(lista_desatualizada)
-
-
