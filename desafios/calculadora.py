e4a1964912dc1433a66b5c5377324e8c3b125a4d
Yarlew <yarlewgabriel03@gmail.com>
2025-01-07 02:55:41 -0300

    quase la

--- desafios/calculadora.py
+++ desafios/calculadora.py
@@ -1,6 +1,6 @@
 import math
 
-numero_pedido=float(input('digite um numero real: '))
-numero_certo=math.floor(numero_pedido)
+numero_pedido = float (input ('digite um numero real: '))
+numero_certo = math.floor (numero_pedido)
 
-print(f'a resposta é {numero_certo}')
+print (f'a resposta é {numero_certo}')
