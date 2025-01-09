256ff32981de6a48429daa27d2597b75996897be
Yarlew <yarlewgabriel03@gmail.com>
2025-01-09 16:02:26 -0300

    Update desafio 01.py

--- desafios/desafio 01.py
+++ desafios/desafio 01.py
@@ -2,4 +2,6 @@ nome=input('qual seu nome ?')
 print('bem vindo',nome,'ao nosso site ;)')
 
 
+
+
 #cria uma script em python que lé o nome de uma pessoae mostre uma mensagem de boas vindas de acordo com o valor (nome do usuario) digitado.
